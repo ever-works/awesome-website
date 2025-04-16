294a3b0feb31fb2810a2f5fb0032ee5c43f3eae2
Paradoxe Ng <paradoxngwasi@gmail.com>
2025-04-16 17:34:10 +0200

    Merge pull request #17 from ever-co/remove-db-var-check
    refactor: remove DATABASE_URL check from drizzle.ts

--- lib/db/drizzle.ts
+++ lib/db/drizzle.ts
@@ -2,9 +2,10 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL environment variable is not set");
-}
+// For now, let's comment this out to avoid a build error in the automated environment (e.g a GH actions)
+// if (!process.env.DATABASE_URL) {
+//   throw new Error("DATABASE_URL environment variable is not set");
+// }
 
 const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
