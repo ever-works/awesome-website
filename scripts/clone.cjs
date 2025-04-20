31e300afa54569fe1a6bd3635d58a15a2a2ef85b
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2025-04-20 05:47:07 +0000

    feat: add script to clone data repository and update tsconfig to include the new script

---
+++ scripts/clone.cjs
@@ -0,0 +1,47 @@
+const { loadEnvConfig } = require("@next/env");
+const git = require("isomorphic-git")
+const http = require("isomorphic-git/http/node")
+const fs = require("fs")
+const path = require("path")
+const os = require('os')
+
+loadEnvConfig(process.cwd());
+
+const token = process.env.GH_TOKEN;
+const url = process.env.DATA_REPOSITORY;
+
+if (!url) {
+  throw new Error("'DATA_REPOSITORY' must be defined as environment variable.");
+}
+
+if (!token) {
+  throw new Error("'GH_TOKEN' must be defined as environment variable.");
+}
+
+
+function getContentPath() {
+  const contentDir = '.content';
+  if (process.env.VERCEL) {
+    return path.join(os.tmpdir(), contentDir);
+  }
+
+  return path.join(process.cwd(), contentDir);
+}
+
+const auth = { username: "x-access-token", password: token };
+const dest = getContentPath();
+
+async function main() {
+  await fs.promises.mkdir(dest, { recursive: true });
+
+  await git.clone({
+    onAuth: () => auth,
+    fs,
+    http,
+    url,
+    dir: dest,
+    singleBranch: true,
+  });
+}
+
+main()
