c0f25a795570d4272f59f4e86c806d99722318fb
Gentil-cizungu <akilimalicizungu268@gmail.com>
2025-04-30 18:23:42 +0200

    feat(auth): add multiple auth providers

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,8 @@
 import type { NextAuthConfig } from "next-auth";
-import { credentialsProvider } from "./lib/auth/credentials";
+import { providers } from "./lib/auth/providers";
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
   trustHost: true,
-  providers: [credentialsProvider],
+  providers: providers,
 } satisfies NextAuthConfig;
