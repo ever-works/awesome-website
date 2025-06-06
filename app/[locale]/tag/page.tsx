0495b083302b05f926026ced3f0d890f9d18b7f1
AKILIMAILI CIZUNGU Innocent <51681130+Innocent-Akim@users.noreply.github.com>
2025-06-06 06:55:08 +0200

    Feat(tags): Add tag navigation and optimize listing (#48)
    * feat(tags): add tag navigation and optimize listing
    
    - Add tag navigation and filtering
    - Create generic SortMenu component for sorting
    - Optimize listing components (better structure, dynamic sorting)
    - Fix typing bugs and clean up code
    - Remove obsolete files and rename for consistency
    
    * add sugg bot
    
    * fix: conflict

---
+++ app/[locale]/tag/page.tsx
@@ -0,0 +1,31 @@
+import { fetchItems } from "@/lib/content";
+import ListingTags from "./listing-tags";
+import { paginateMeta } from "@/lib/paginate";
+import { LOCALES } from "@/lib/constants";
+
+export const revalidate = 10;
+export async function generateStaticParams() {
+  return LOCALES.map((locale) => ({ locale }));
+}
+
+export default async function TagIndexPage({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } =await params
+  const { start, page } = paginateMeta();
+  const { tags, total, items, categories } = await fetchItems({ lang: locale });
+
+  return (
+      <ListingTags
+        total={total}
+        start={start}
+        page={page}
+        basePath="/tag/paging"
+        categories={categories}
+        tags={tags}
+        items={items}
+      />
+  );
+} 
---
+++ app/[locale]/tag/paging/page.tsx
@@ -0,0 +1,31 @@
+import { fetchItems } from "@/lib/content";
+import { paginateMeta } from "@/lib/paginate";
+import { LOCALES } from "@/lib/constants";
+import ListingTags from "../listing-tags";
+
+export const revalidate = 10;
+export async function generateStaticParams() {
+  return LOCALES.map((locale) => ({ locale }));
+}
+
+export default async function TagPagingPage({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } =await params
+  const { start, page } = paginateMeta();
+  const { tags, total, items, categories } = await fetchItems({ lang: locale });
+
+  return (
+      <ListingTags
+        total={total}
+        start={start}
+        page={page}
+        basePath="/tag/paging"
+        categories={categories}
+        tags={tags}
+        items={items}
+      />
+  );
+} 
