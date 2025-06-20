7c737082ad007b030ac7eaa89c15975b06717f1f
Innocent-Akim <gentilakili98@gmail.com>
2025-06-20 12:22:22 +0200

    fix: build

--- app/[locale]/(listing)/globals-client.tsx
+++ app/[locale]/(listing)/globals-client.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useLayoutTheme } from "@/components/context";
+import { LayoutHome, useLayoutTheme } from "@/components/context";
 import { Categories, Paginate, Tags } from "@/components/filters";
 import { Tag, Category, ItemData } from "@/lib/content";
 import { sortByNumericProperty } from "@/lib/utils";
@@ -18,12 +18,12 @@ type ListingProps = {
 };
 
 export default function GlobalsClient(props: ListingProps) {
-  const { layoutHome = "Home_1" } = useLayoutTheme();
+  const { layoutHome =LayoutHome.HOME_ONE } = useLayoutTheme();
   const homeTwoLogic = useHomeTwoLogic(props);
   const sortedTags = sortByNumericProperty(props.tags);
   const sortedCategories = sortByNumericProperty(props.categories);
 
-  if (layoutHome === "Home_1") {
+  if (layoutHome === LayoutHome.HOME_ONE) {
     return (
       <div className="pb-12">
         <div className="flex flex-col md:flex-row w-full gap-5">
