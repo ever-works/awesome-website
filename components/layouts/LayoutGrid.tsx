d052363ef328cccea22d80eef891aeaf13636b72
Innocent-Akim <gentilakili98@gmail.com>
2025-06-23 15:52:05 +0200

    feat(views): implement layout switcher with list, grid, and masonry views     - **List View ():** A standard vertical list for detailed scanning.     - **Grid View ():** A uniform grid where items are stretched for alignment.     - **Masonry View ():** A dynamic masonry layout for a visually engaging presentation.

--- components/layouts/LayoutGrid.tsx
+++ components/layouts/LayoutGrid.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function LayoutGrid({ children }: { children: ReactNode }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 sm:gap-5 lg:gap-6">
+    <div className="grid justify-items-stretch grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 sm:gap-5 lg:gap-6">
       {children}
     </div>
   );
--- components/layouts/index.ts
+++ components/layouts/index.ts
@@ -2,13 +2,15 @@ import LayoutClassic from "./LayoutClassic";
 import LayoutGrid from "./LayoutGrid";
 import LayoutCards from "./LayoutCards";
 import { JSX } from "react";
+import LayoutMasonry from "./LayoutMasonry";
 
-export type LayoutKey = 'classic' | 'grid' | 'cards';
+export type LayoutKey = 'classic' | 'grid' | 'cards' | 'masonry';
 
 export const layoutComponents: Record<LayoutKey, ({ children }: { children: React.ReactNode }) => JSX.Element> = {
   grid: LayoutGrid,
   cards: LayoutCards,
   classic: LayoutClassic,
+  masonry: LayoutMasonry,
 };
 
 export { LayoutClassic, LayoutGrid, LayoutCards };
